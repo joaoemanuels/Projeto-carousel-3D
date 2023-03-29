af2ddc438767dfdba6f88ffc745aa8f21970739d
João Emanuel <jemanuel.pi@gmail.com>
2023-03-29 11:12:39 -0300

    carousel-3D

--- assets/scripts/card-animation.js
+++ assets/scripts/card-animation.js
@@ -10,7 +10,7 @@ function handleMouseLeave() {
 function addEventListernersToCards() {
   const cardElements = document.getElementsByClassName('s-card');
 
-  for(let index = 0; index < cardElements.length; index++) {
+for(let index = 0; index < cardElements.length; index++) {
     const card = cardElements[index];
     card.addEventListener('mouseenter', handleMouseEnter);
     card.addEventListener('mouseleave', handleMouseLeave);
